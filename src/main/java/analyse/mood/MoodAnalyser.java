b754d948eaa71c7d53ffa35cff87879c74e81031
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-02 02:21:18 +0530

    Added: Welcome message!

---
+++ src/main/java/analyse/mood/MoodAnalyser.java
@@ -0,0 +1,8 @@
+package analyse.mood;
+
+public class MoodAnalyser {
+
+    String analyseMood(String message) {
+        return message;
+    }
+}
---
+++ src/main/java/analyse/mood/MoodAnalyserMain.java
@@ -0,0 +1,7 @@
+package analyse.mood;
+
+public class MoodAnalyserMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Mood Analyser Program!iiiiiiZZ");
+    }
+}
