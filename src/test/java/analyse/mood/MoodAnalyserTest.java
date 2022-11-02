f9bb373f1ebd925a448567ba736fc4640829b12a
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-02 10:12:08 +0530

    Added: Testing HAPPY mood

--- src/test/java/analyse/mood/MoodAnalyserTest.java
+++ src/test/java/analyse/mood/MoodAnalyserTest.java
@@ -7,7 +7,7 @@ public class MoodAnalyserTest {
 
     @Test
     public void analyseMoodTest() {
-        MoodAnalyser analyser = new MoodAnalyser("I am in Sad Mood");
-        Assert.assertEquals("SAD", analyser.analyseMood());
+        MoodAnalyser analyser = new MoodAnalyser("I am in Happy Mood");
+        Assert.assertEquals("HAPPY", analyser.analyseMood());
     }
 }
